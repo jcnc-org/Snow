4f1b2a4accdcdfa01fa2e7560c2c8c1bc3d82594
Luke <luke.k.xu@hotmail.com>
2025-11-12 23:26:25 +0800

    feat: 优化IR上下文中的标签生成逻辑
    - 将标签生成方式从简单的"L0", "L1"改为包含函数名前缀的形式
    - 确保生成的标签在不同函数间具有唯一性
    - 提高中间代码的可读性和调试便利性

--- src/main/java/org/jcnc/snow/compiler/ir/builder/core/IRContext.java
+++ src/main/java/org/jcnc/snow/compiler/ir/builder/core/IRContext.java
@@ -99,7 +99,7 @@ public class IRContext {
      * @return 形如 "L0", "L1" 等的唯一字符串标签
      */
     public String newLabel() {
-        return "L" + (labelCounter++);
+        return function.name() + "$L" + (labelCounter++);
     }
 
     /**
